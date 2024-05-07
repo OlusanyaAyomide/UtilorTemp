2284b1e21a1bf8440f581165ca695cd93619fa42
Olusanya Ayomide <ayomideflex72@gmail.com>
2024-05-07 11:51:36 +0100

    cookie-fix-progress

--- build/utils/CookieService.js
+++ build/utils/CookieService.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setCookie = void 0;
 var setCookie = function (_a) {
-    var name = _a.name, value = _a.value, _b = _a.duration, duration = _b === void 0 ? 30 : _b, res = _a.res;
+    var name = _a.name, value = _a.value, _b = _a.duration, duration = _b === void 0 ? 3600 : _b, res = _a.res;
     var currentTime = new Date();
     var futureTime = new Date(currentTime.getTime() + (duration * 60000));
     var isProd = process.env.APP_ENV !== "DEV";
